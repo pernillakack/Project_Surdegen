3bb6317946796e8a202d858baf52c528a8be9d8f
pernillakack <112933591+pernillakack@users.noreply.github.com>
2023-01-08 10:37:49 +0100

    Menu-button hides when clicked

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
-const hide_messageEl = document.getElementById("hide_message");
-const contactsEl = document.getElementById("contacts");
-
-
-function hideMessage() {
-    
-    contactsEl.classList.toggle("hide");
-    console.log("Meddelandet sänds och formuläret göms.");
-    
-}
-hide_messageEl.addEventListener('click', hideMessage);
-  
-//Dölj menyn
 const menuButtonEl = document.getElementById("menuButton");
 const menuEl = document.getElementById("menu");
 
 function showMenu() {
+    console.log("Hej");
     menuEl.classList.toggle("displayMenu");
+    menuButtonEl.classList.add("hide");
+    console.log("Öppnar menyn...");
 }
 menuButtonEl.addEventListener('click', showMenu);
+
+
+const hide_messageEl = document.getElementById("hide_message");
+const contactsEl = document.getElementById("contacts");
+const sentEl = document.getElementById("sent")
+        
+        
+        function hideMessage() {
+            console.log("Hejjj!");
+            contactsEl.classList.add("hide");
+            sentEl.classList.add(showMessage);
+            console.log("Meddelandet sänds och formuläret göms.");
+            
+        }
+        hide_messageEl.addEventListener('click', hideMessage);
