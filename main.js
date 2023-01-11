887a52e69eccf668d325069bc7bda55ea74704d5
pernillakack <112933591+pernillakack@users.noreply.github.com>
2023-01-11 09:27:18 +0100

    ToTheTop-button

--- main.js
+++ main.js
@@ -19,20 +19,9 @@ function hideMessage() {
     sentEl.classList.add("showMessage");
     console.log("Meddelandet sänds och formuläret göms.");
 }
-/*hide_messageEl.addEventListener('click', hideMessage);
+hide_messageEl.addEventListener('click', hideMessage);
 
 hide_messageEl.addEventListener('click', (event) => {
     // Prevent the form from submitting
     event.preventDefault();
 });
-
-
-
-
-const topButtonEl = document.getElementById("toTop");
-
-function backToTop() {
-    console.log("Tillbaka till toppen av sidan");
-
-}
-topButtonEl.addEventListener('click', backToTop);*/
